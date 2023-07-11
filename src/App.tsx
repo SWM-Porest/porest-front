3c7a35c5442f23952e8d60da76a5f414dc84cab5
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-07-11 09:33:10 +0900

    test
    test upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import Cart from 'Pages/Cart'
+import { Route, Routes } from 'react-router-dom'
+//import './assets/scss/common.scss'
+
 function App() {
-  return <div className="App">:)</div>
+  return (
+    <div className="App">
+      :) test
+      <Routes>
+        <Route path="/" element={<Cart />} />
+      </Routes>
+    </div>
+  )
 }
 
 export default App
