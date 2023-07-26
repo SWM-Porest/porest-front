f33a673de0c97398838fc9a944beb76b0fc08f3b
kimhyun5u <gustndlfrl@gmail.com>
2023-07-26 20:17:18 +0900

    Merge branch 'dev' into 'smart-header'

--- src/Component/Header.tsx
+++ src/Component/Header.tsx
@@ -3,14 +3,16 @@ import { FlexAlignCSS } from 'Styles/common'
 import styled from 'styled-components'
 
 interface HeaderProps {
+  Left: React.ReactNode | undefined
   HeaderName: string
+  Right: React.ReactNode | undefined
 }
 
-const Header: React.FC<HeaderProps> = ({ HeaderName }) => {
+const Header: React.FC<HeaderProps> = ({ Left, HeaderName, Right }) => {
   const { openModal } = useCartModal()
   return (
     <StyledNavbar>
-      <span style={{ width: 120 }} />
+      {Left && <span style={{ width: 120 }} />}
       <h2 style={{ margin: 0 }}>{HeaderName}</h2>
       <StyledButton onClick={openModal}>
         <h5 style={{ margin: 0 }}>주문내역</h5>
