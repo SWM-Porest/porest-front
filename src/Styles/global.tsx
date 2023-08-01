a86dd7e6cfc10af8a6899d0b8168e73e3dc0b3f0
kimhyun5u <gustndlfrl@gmail.com>
2023-08-01 23:02:59 +0900

    style: remove browser default css effect

--- src/Styles/global.tsx
+++ src/Styles/global.tsx
@@ -18,6 +18,7 @@ body {
     border: none;
     outline: none;
     list-style: none;
+    -webkit-tap-highlight-color : transparent; // 모바일에서 클릭 시 하이라이트 제거
 }
 
 h1 {
@@ -52,5 +53,17 @@ div {
     font-family: 'Noto Sans KR', sans-serif;
 }
 
+// 브라우저 별 기본 효과 제거
+input, textarea, button {
+    appearance: none;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    border-radius: 0;
+    -webkit-border-radius: 0;
+    -moz-border-radius: 0;
+}
 
+input:focus{
+	outline:none;
+}
 `
