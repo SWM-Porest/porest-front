7f4baf3f03ead467300a4f3bf158801520a7ff81
Kim Hyunsu <38347891+kimhyun5u@users.noreply.github.com>
2023-08-03 20:37:26 +0900

    Revert "update: update height measure % to vh"

--- src/Component/MenuComponent/MenuModal.tsx
+++ src/Component/MenuComponent/MenuModal.tsx
@@ -93,7 +93,7 @@ const ModalContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 100%;
 `
 const ModalBackdrop = styled.div<{ $load: boolean }>`
   z-index: 30;
