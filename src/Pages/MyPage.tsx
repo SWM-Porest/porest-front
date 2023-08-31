db6f6994a4a338dd660bab4fd406c3794eda5523
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-08-31 11:41:13 +0900

    feat: add MyPage

---
+++ src/Pages/MyPage.tsx
@@ -0,0 +1,13 @@
+import { useAuth } from 'Context/AuthContext'
+import Header from '../Component/Header'
+
+const MyPage = () => {
+  const { accessToken } = useAuth() // accessToken 사용 법!
+
+  return (
+    <div>
+      <Header HeaderName="마이 페이지" Right={''} />
+    </div>
+  )
+}
+export default MyPage
