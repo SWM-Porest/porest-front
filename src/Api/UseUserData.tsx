a018afce7f09c7ad254edcf18d5eec609afe0b85
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-09-04 21:17:47 +0900

    feat: add get user data by react query, accessToken

---
+++ src/Api/UseUserData.tsx
@@ -0,0 +1,25 @@
+import axios from 'axios'
+import { useQuery } from 'react-query'
+
+// 사용자 데이터를 가져오는 함수를 정의합니다.
+const fetchUserData = async (userId: string, page: number, pageSize: number, sort: string, accessToken: string) => {
+  const response = await axios.get(
+    `${process.env.REACT_APP_API_URL}/order/user/${userId}?page=${page}&pageSize=${pageSize}&sort=${sort}`,
+    {
+      headers: {
+        Authorization: `Bearer ${accessToken}`, // accessToken을 헤더에 추가합니다.
+      },
+    },
+  )
+
+  return response.data
+}
+
+// 사용자 데이터를 가져오는 React Query 훅을 사용합니다.
+const UseUserData = (userId: string, page: number, pageSize: number, sort: string, accessToken: string) => {
+  return useQuery(['userData', userId, page, pageSize, sort], () =>
+    fetchUserData(userId, page, pageSize, sort, accessToken),
+  )
+}
+
+export default UseUserData
