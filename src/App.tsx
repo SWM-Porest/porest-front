2bf2ca89612c21d6c432fc54800f65a83476e866
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-09-07 08:38:57 +0900

    fix: Structural changes for easy route management, private route implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,28 @@
-import { AuthProvider } from 'Context/AuthContext'
 import { CartModalProvider } from 'Context/CartModalContext'
 import { RestaurantProvider } from 'Context/restaurantContext'
-import EditRestaurantPage from 'Pages/EditRestaurantPage'
-import ErrorPage from 'Pages/ErrorPage'
-import MenuBoardPage from 'Pages/MenuBoardPage'
-import ProfilePage from 'Pages/ProfilePage'
-import RestaurantListPage from 'Pages/RestaurantListPage'
-import RestaurantPage from 'Pages/RestaurantPage'
+import router from 'Routes/router'
 import { GlobalStyles } from 'Styles/global'
 import { theme } from 'Styles/theme'
 import React from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
-import { Route, Routes, useLocation } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import { RecoilRoot } from 'recoil'
 import { ThemeProvider } from 'styled-components'
-import LoginPage from './Pages/LoginPage'
-import PrivateRoute from './PrivateRoute' // PrivateRoute 가져오기
 
 const App: React.FC = () => {
-  const location = useLocation()
-  const errorCode = location.state && location.state.errorCode ? location.state.errorCode : 404
   const queryClient = new QueryClient()
 
   return (
     <QueryClientProvider client={queryClient}>
       <ThemeProvider theme={theme}>
-        <AuthProvider>
+        <RecoilRoot>
           <GlobalStyles />
           <CartModalProvider>
             <RestaurantProvider>
-              <Routes>
-                <Route path="/login" element={<LoginPage />} />
-                <Route path="/restaurants/list" element={<RestaurantListPage />} />
-                <Route path="/restaurants/:id" element={<MenuBoardPage />} />
-                <Route path="/restaurants/:id/info" element={<RestaurantPage />} />
-                {/* PrivateRoute로 래핑된 컴포넌트를 <Route>로 사용 */}
-                <Route
-                  path="/restaurants/:id/edit"
-                  element={<PrivateRoute path="/restaurants/:id/edit" element={<EditRestaurantPage />} />}
-                />
-                <Route path="/mypage" element={<PrivateRoute path="/mypage" element={<ProfilePage />} />} />
-                <Route path="*" element={<ErrorPage errorCode={errorCode} />} />
-              </Routes>
+              <RouterProvider router={router} />
             </RestaurantProvider>
           </CartModalProvider>
-        </AuthProvider>
+        </RecoilRoot>
       </ThemeProvider>
     </QueryClientProvider>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
 import { CookiesProvider } from 'react-cookie'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <CookiesProvider>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </CookiesProvider>,
 )
