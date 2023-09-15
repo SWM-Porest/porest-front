bb5887660e18cf536cb47c6f0e54aa25ac24114b
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-09-15 22:40:10 +0900

    feat: OrderModal 전체적인 구조 변경

--- src/Component/Modal/OrderModal.tsx
+++ src/Component/Modal/OrderModal.tsx
@@ -1,9 +1,12 @@
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 import Header from 'Component/Header'
 import { CloseButton, CloseButtonContainer } from 'Component/Modal/CartModal'
+import formatDate from 'Component/formatDate'
 import { Image } from 'Context/restaurantContext'
+import { Steps } from 'antd'
 import React from 'react'
 import styled from 'styled-components'
+const { Step } = Steps
 
 interface Menu {
   menu_name: string
@@ -13,12 +16,14 @@ interface Menu {
 }
 
 interface Order {
-  restaurant_id: number
+  restaurant_id: string
   restaurant_name: string
+  restaurant_address: string
   updated_at: string
   _id: string
-  menus: Menu[]
+  menus: { [menuId: string]: Menu }
   status: number
+  status_updated_at: { [status: string]: string }
 }
 
 interface OwnProps {
@@ -33,7 +38,27 @@ const OrderModal: React.FC<OwnProps> = ({ order, isOpen, openModalHandler }) => 
   } else {
     document.body.style.overflow = 'auto'
   }
-  console.log(order)
+
+  const getStatusText = (status: number) => {
+    switch (status) {
+      case 1:
+        return '접수중'
+      case 2:
+        return '준비중'
+      case 3:
+        return '준비 완료'
+      case 4:
+        return '서빙 완료'
+      case 5:
+        return '결제 완료'
+      default:
+        return '상태 없음'
+    }
+  }
+  const shouldShowStep = (stepStatus: number) => {
+    return order.status >= stepStatus
+  }
+
   return (
     <>
       <ModalContainer>
@@ -61,36 +86,41 @@ const OrderModal: React.FC<OwnProps> = ({ order, isOpen, openModalHandler }) => 
           ></Header>
 
           <ContentContainer>
-            <StatusText visible={order.status >= 1} color={order.status >= 2 ? getStatusColor(2) : getStatusColor(1)}>
-              {getStatusText(1)}
-            </StatusText>
-            <StatusText visible={order.status >= 2} color={order.status >= 3 ? getStatusColor(2) : getStatusColor(1)}>
-              {getStatusText(2)}
-            </StatusText>
-            <StatusText visible={order.status >= 3} color={order.status >= 4 ? getStatusColor(2) : getStatusColor(1)}>
-              {getStatusText(3)}
-            </StatusText>
-            <StatusText visible={order.status >= 4} color={order.status >= 5 ? getStatusColor(2) : getStatusColor(1)}>
-              {getStatusText(4)}
-            </StatusText>
-            <StatusText visible={order.status >= 5} color={order.status >= 6 ? getStatusColor(2) : getStatusColor(1)}>
-              {getStatusText(5)}
-            </StatusText>
+            <CustomSteps direction="vertical" current={order.status - 1}>
+              {shouldShowStep(1) && (
+                <Step title={getStatusText(1)} description={formatDate(`${order.status_updated_at[1]}`)} />
+              )}
+              {shouldShowStep(2) && (
+                <Step title={getStatusText(2)} description={formatDate(`${order.status_updated_at[2]}`)} />
+              )}
+              {shouldShowStep(3) && (
+                <Step title={getStatusText(3)} description={formatDate(`${order.status_updated_at[3]}`)} />
+              )}
+              {shouldShowStep(4) && (
+                <Step title={getStatusText(4)} description={formatDate(`${order.status_updated_at[4]}`)} />
+              )}
+              {shouldShowStep(5) && (
+                <Step title={getStatusText(5)} description={formatDate(`${order.status_updated_at[5]}`)} />
+              )}
+            </CustomSteps>
 
             <OrderInfoContainer>
               <div>
                 <OrderInfoLabel>주문 매장</OrderInfoLabel>
-                <OrderInfoValue>{order.restaurant_name}</OrderInfoValue>
+                <OrderInfoValue>
+                  <OrderRestaurantName>{order.restaurant_name}</OrderRestaurantName>
+                  <OrderRestaurantAddress>{order.restaurant_address}</OrderRestaurantAddress>
+                </OrderInfoValue>
               </div>
               {Object.values(order.menus).map((menu, index) => (
                 <MenuItem key={index}>
                   <MenuName>{menu.menu_name}</MenuName>
-                  <MenuPrice>{menu.price}원</MenuPrice>
-                  <MenuQuantity>x{menu.quantity}</MenuQuantity>
+                  <MenuPrice>{menu.price.toLocaleString()}원</MenuPrice>
+                  <MenuQuantity>x {menu.quantity}</MenuQuantity>
                 </MenuItem>
               ))}
               <TotalPrice>
-                총 금액:{' '}
+                <div>총 금액: </div>
                 <ColoredText>
                   {Object.values(order.menus)
                     .reduce((menuTotal: number, menu: Menu) => menuTotal + menu.price * menu.quantity, 0)
@@ -123,7 +153,7 @@ const ModalBackdrop = styled.div<{ $load: boolean }>`
   display: flex;
   justify-content: center;
   align-items: flex-end;
-  background-color: rgba(0, 0, 0, 0.4);
+  background-color: ${({ theme }) => theme.COLOR.common.gray[600]};
   ${(props) => (props.$load ? 'top: 0; left: 0; right: 0;' : '')};
 `
 
@@ -134,97 +164,91 @@ const ModalView = styled.div<{ $load: boolean }>`
   border-radius: 40px 40px 0px 0px;
   width: 100%;
   height: 80%;
-  background-color: #ffffff;
+  background-color: ${({ theme }) => theme.COLOR.common.white};
   transition: all 0.6s cubic-bezier(0.22, 0.61, 0.36, 1);
 `
 
 const ContentContainer = styled.div`
   overflow-y: auto;
   max-height: calc(100% - 72px - 72pt);
-  padding: 20px;
+  padding: 0 48pt;
 `
 
 const OrderInfoContainer = styled.div`
-  margin-top: 20px;
+  margin-top: 16pt;
 `
 
 const OrderInfoLabel = styled.div`
-  font-weight: bold;
-  margin-bottom: 5px;
+  padding: 8pt;
+
+  font-size: 2rem;
+  background-color: ${({ theme }) => theme.COLOR.common.gray[700]};
 `
 
 const OrderInfoValue = styled.div`
-  color: #555;
+  color: ${({ theme }) => theme.COLOR.common.gray[200]};
+  padding: 8pt;
+  border-top: 1px solid #ddd;
+  border-bottom: 1px solid #ddd;
+`
+const OrderRestaurantName = styled.div`
+  padding-top: 8pt;
+  font-size: 2rem;
+  font-weight: bold;
+`
+const OrderRestaurantAddress = styled.div`
+  padding: 4pt 0 8pt 0;
+  font-size: 1.8rem;
 `
 
 const MenuItem = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin-top: 10px;
+  padding: 8pt;
+  font-size: 2rem;
 `
 
 const MenuName = styled.div`
   flex: 1;
+  color: ${({ theme }) => theme.COLOR.common.gray[200]};
+  font-weight: bold;
 `
 
 const MenuPrice = styled.div`
-  margin-left: 10px;
+  margin-left: 16pt;
+  color: ${({ theme }) => theme.COLOR.common.gray[700]};
 `
 
 const MenuQuantity = styled.div`
-  margin-left: 10px;
-  color: #555;
+  margin-left: 16pt;
+  color: ${({ theme }) => theme.COLOR.common.gray[700]};
 `
 
 const TotalPrice = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 8pt;
+
+  font-size: 2rem;
+  background-color: ${({ theme }) => theme.COLOR.common.gray[700]};
+
+  border-top: 1px solid #ddd;
+
   font-weight: bold;
-  margin-top: 20px;
 `
 
 const ColoredText = styled.span`
   color: ${({ theme }) => theme.COLOR.main};
 `
+const CustomSteps = styled(Steps)`
+  .ant-steps-item {
+    font-family: 'Noto Sans KR', sans-serif;
+    font-weight: normal;
+  }
 
-const StatusText = styled.div<{ visible: boolean; color: string }>`
-  font-weight: bold;
-  padding: 4px 8px;
-  border-radius: 4px;
-  visibility: ${({ visible }) => (visible ? 'visible' : 'hidden')};
-  color: ${({ color }) => (color === 'gray' ? 'blue' : 'gray')};
-  background-color: ${({ color }) => (color === 'gray' ? 'lightblue' : 'lightgray')};
+  .ant-steps-item-process .ant-steps-item-title {
+    font-weight: bold;
+  }
 `
-
-const getStatusColor = (status: number) => {
-  switch (status) {
-    case 1:
-      return 'gray'
-    case 2:
-      return 'yellow'
-    case 3:
-      return 'green'
-    case 4:
-      return 'blue'
-    case 5:
-      return 'purple'
-    default:
-      return 'gray'
-  }
-}
-
-const getStatusText = (status: number) => {
-  switch (status) {
-    case 1:
-      return '접수중'
-    case 2:
-      return '준비중'
-    case 3:
-      return '준비 완료'
-    case 4:
-      return '서빙 완료'
-    case 5:
-      return '결제 완료'
-    default:
-      return '상태 없음'
-  }
-}
