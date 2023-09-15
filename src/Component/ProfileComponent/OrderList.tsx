7a6059a452cc0432de336a3278fe1cd3b0f5933b
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-09-15 09:43:29 +0900

    feat: 주문 내역 페이지

--- src/Component/ProfileComponent/OrderList.tsx
+++ src/Component/ProfileComponent/OrderList.tsx
@@ -1,25 +1,49 @@
 import { useAccessToken } from 'Api/tokenCookie'
 import useUserData from 'Api/useUserData'
+import useUserOrderData from 'Api/useUserOrderData'
+import OrderModal from 'Component/Modal/OrderModal'
+import getImageSrc from 'Component/getImageSrc'
+import { Image } from 'Context/restaurantContext'
 import { Table } from 'antd'
+import { useState } from 'react'
 import styled from 'styled-components'
-interface Item {
+interface Menu {
   menu_name: string
   price: number
+  quantity: number
+  img: Image
 }
 
 interface Order {
-  restaurant: string
-  date: string
-  items: Item[]
+  restaurant_id: number
+  restaurant_name: string
+  updated_at: string
+  _id: string
+  menus: Menu[]
+  status: number
 }
 
 const OrderList = () => {
   const page = 1 // page 변수 정의
   const pageSize = 10 // pageSize 변수 정의
-  const sort = 'asc' // sort 변수 정의
+  const sort = 0 // sort 변수 정의
 
   const [accessToken, setAccessToken] = useAccessToken()
-  const { data: userData, isLoading, isError } = useUserData(page, pageSize, sort, accessToken)
+  const [orderId, setOrderId] = useState('')
+  const [isOpen, setIsOpen] = useState(false)
+
+  const { data: userOrderData, isLoading, isError } = useUserOrderData(page, pageSize, sort, accessToken)
+  const { data: userData } = useUserData(accessToken)
+
+  const openModalHandler = (id: string) => {
+    setOrderId(id)
+    setIsOpen(!isOpen)
+  }
+
+  const formatDate = (dateString: string) => {
+    const date = new Date(dateString)
+    return `${date.getFullYear()}.${date.getMonth() + 1}.${date.getDate()} ${date.getHours()}:${date.getMinutes()}`
+  }
 
   if (isLoading) {
     return <div>Loading...</div>
@@ -28,22 +52,27 @@ const OrderList = () => {
   if (isError) {
     return <div>Error fetching data</div>
   }
-  // 매장별 주문 회수 통계를 생성
-  const restaurantOrders: Record<string, number> = {}
-  if (userData) {
-    userData.orders.forEach((order: Order) => {
-      // 매장 이름을 키로 사용하여 주문 횟수를 누적
-      if (order.restaurant in restaurantOrders) {
-        restaurantOrders[order.restaurant]++
-      } else {
-        restaurantOrders[order.restaurant] = 1
-      }
-    })
+
+  if (!userData || !userOrderData || !Array.isArray(userOrderData.orders)) {
+    return <div>No orders found.</div>
   }
 
-  const restaurantOrderDataSource = Object.keys(restaurantOrders).map((restaurant) => ({
-    restaurant,
-    orderCount: restaurantOrders[restaurant],
+  const restaurantOrders: Record<string, { name: string; count: number }> = {}
+  userOrderData.orders.forEach((order: Order) => {
+    const { restaurant_id, restaurant_name } = order
+    if (restaurant_id in restaurantOrders) {
+      restaurantOrders[restaurant_id].count++
+    } else {
+      restaurantOrders[restaurant_id] = {
+        name: restaurant_name,
+        count: 1,
+      }
+    }
+  })
+  const restaurantOrderDataSource = Object.keys(restaurantOrders).map((restaurant_id) => ({
+    key: restaurant_id,
+    restaurant: restaurantOrders[restaurant_id].name,
+    orderCount: restaurantOrders[restaurant_id].count,
   }))
 
   const restaurantOrderColumns = [
@@ -56,46 +85,88 @@ const OrderList = () => {
       title: '주문 횟수',
       dataIndex: 'orderCount',
       key: 'orderCount',
+      render: (text: number) => <span>{text}회</span>,
     },
   ]
+
+  const totalPrice = userOrderData.orders.reduce((total: number, order: Order) => {
+    if (typeof order.menus === 'object') {
+      const orderTotal = Object.values(order.menus).reduce((menuTotal, menu) => {
+        return menuTotal + menu.price * menu.quantity
+      }, 0)
+      return total + orderTotal
+    }
+    return total
+  }, 0)
 
   return (
     <div>
       <div>
-        {userData ? (
+        {userData && userOrderData ? (
           <StyledContainer>
-            <h5>
-              {userData.name}님은 <StyledColor>{userData.orders.length}</StyledColor>회 주문하셨고,
-            </h5>
-            <h5>
-              <StyledColor>
-                {userData.orders
-                  .reduce(
-                    (totalAmount: number, order: Order) =>
-                      totalAmount + order.items.reduce((itemTotal: number, item: Item) => itemTotal + item.price, 0),
-                    0,
-                  )
-                  .toLocaleString()}
-              </StyledColor>
-              원 결제하셨어요.
-            </h5>
+            <StyledTitle>
+              {userData.nickname}님은 <ColoredText>{userOrderData.orders.length}</ColoredText>회 주문하셨고,
+            </StyledTitle>
+            <StyledTitle>
+              <ColoredText>{totalPrice.toLocaleString()}</ColoredText>원 결제하셨어요.
+            </StyledTitle>
             <StyledTable columns={restaurantOrderColumns} dataSource={restaurantOrderDataSource} pagination={false} />
 
-            {userData.orders.map((order: Order, index: number) => (
-              <OrderDetails key={index}>
-                <StyledRestaurant>매장: {order.restaurant}</StyledRestaurant>
-                <StyledTime>구매일시: {order.date}</StyledTime>
-                {order.items.map((item, itemIndex) => (
-                  <StyledOrderContainer key={itemIndex}>
-                    <p>{item.menu_name}</p>
-                    <p>{item.price.toLocaleString()}원</p>
-                  </StyledOrderContainer>
-                ))}
-                <StyledOrderContainer>
-                  <p>총 가격:</p>
-                  <p>{order.items.reduce((itemTotal, item) => itemTotal + item.price, 0).toLocaleString()}원</p>
-                </StyledOrderContainer>
-              </OrderDetails>
+            {userOrderData.orders.map((order: Order) => (
+              <div key={order._id}>
+                <OrderModal
+                  order={order}
+                  isOpen={isOpen && orderId === order._id}
+                  openModalHandler={() => {
+                    openModalHandler(order._id)
+                  }}
+                />
+                <OrderDetails>
+                  <MenuImageContainer>
+                    {Object.values(order.menus)
+                      .slice(0, 1)
+                      .map((menu: Menu, menuIndex: number) => (
+                        <MenuImage key={menuIndex} src={getImageSrc(menu.img)} alt="메뉴 이미지" />
+                      ))}
+                  </MenuImageContainer>
+                  <MenuDetailsContainer
+                    key={`menu_${order._id}`}
+                    onClick={() => {
+                      openModalHandler(order._id)
+                    }}
+                  >
+                    {Object.values(order.menus)
+                      .slice(0, 1)
+                      .map((menu: Menu, menuIndex: number) => (
+                        <MenuNameContainer key={menuIndex}>
+                          <div>
+                            {Object.values(order.menus).length > 1 ? (
+                              <div>
+                                {menu.menu_name} 외 {Object.values(order.menus).length - 1}개
+                              </div>
+                            ) : (
+                              <div> {menu.menu_name} </div>
+                            )}
+                          </div>
+                        </MenuNameContainer>
+                      ))}
+
+                    <MenuInfoContainer>
+                      <MenuDateContainer>{formatDate(order.updated_at)}</MenuDateContainer>
+
+                      <div>
+                        <RestaurantNameContainer>{order.restaurant_name}</RestaurantNameContainer>
+                        <ColoredText>
+                          {Object.values(order.menus)
+                            .reduce((menuTotal: number, menu: Menu) => menuTotal + menu.price * menu.quantity, 0)
+                            .toLocaleString()}
+                          원
+                        </ColoredText>
+                      </div>
+                    </MenuInfoContainer>
+                  </MenuDetailsContainer>
+                </OrderDetails>
+              </div>
             ))}
           </StyledContainer>
         ) : (
@@ -110,8 +181,17 @@ const OrderList = () => {
 
 export default OrderList
 
+const StyledTitle = styled.div`
+  font-size: 2.5rem;
+  font-weight: bold;
+  margin-bottom: 8pt;
+  cursor: default;
+  padding-left: 8pt;
+`
+
 const StyledTable = styled(Table)`
   margin-top: 24pt;
+  margin-bottom: 24pt;
 
   th {
     font-size: 2rem;
@@ -131,24 +211,52 @@ const StyledContainer = styled.div`
 
 const OrderDetails = styled.div`
   border: 1px solid #ddd;
-  border-radius: 4pt;
-  padding: 16pt;
-  margin-top: 24pt;
+
+  height: 160pt;
+  display: flex;
+  padding: 24pt 48pt;
+  justify-content: space-between;
+  cursor: pointer;
+`
+const MenuImageContainer = styled.div`
+  position: relative;
+  width: 112pt;
+  padding-top: 112pt;
 `
 
-const StyledColor = styled.span`
+const MenuDetailsContainer = styled.div`
+  padding-left: 24pt;
+  width: 80%;
+`
+const MenuNameContainer = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+  padding: 16pt 0;
+`
+
+const MenuDateContainer = styled.div`
+  color: ${({ theme }) => theme.COLOR.common.gray[400]};
+`
+
+const RestaurantNameContainer = styled.span`
+  padding-right: 8pt;
+`
+const ColoredText = styled.span`
   color: ${({ theme }) => theme.COLOR.main};
 `
 
-const StyledRestaurant = styled.p`
-  color: ${({ theme }) => theme.COLOR.common.gray[200]};
-  font-size: 2rem;
-  font-weight: bold;
-`
-const StyledTime = styled.p`
-  color: ${({ theme }) => theme.COLOR.common.gray[500]};
-`
-const StyledOrderContainer = styled.div`
+const MenuInfoContainer = styled.div`
   display: flex;
+  padding: 8pt 0;
   justify-content: space-between;
+  font-size: 1.8rem;
+`
+
+const MenuImage = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  border-radius: 50%;
 `
