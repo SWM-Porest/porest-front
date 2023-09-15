d45a39e4e6f51b888e1f4666a536ce11595e0253
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-09-15 09:42:54 +0900

    feat: 사용자 데이터, 사용자 주문내역 데이터 가져오기

--- src/Api/UseUserData.tsx
+++ src/Api/UseUserData.tsx
@@ -2,22 +2,18 @@ import axios from 'axios'
 import { useQuery } from 'react-query'
 
 // 사용자 데이터를 가져오는 함수
-const fetchUserData = async (page: number, pageSize: number, sort: string, accessToken: string) => {
-  const response = await axios.get(
-    `${process.env.REACT_APP_API_URL}/order/user?page=${page}&pageSize=${pageSize}&sort=${sort}`,
-    {
-      headers: {
-        Authorization: `Bearer ${accessToken}`,
-      },
+const fetchUserData = async (accessToken: string) => {
+  const response = await axios.get(`${process.env.REACT_APP_API_URL}/auth/user`, {
+    headers: {
+      Authorization: `Bearer ${accessToken}`,
     },
-  )
+  })
 
   return response.data
 }
-
 // 사용자 데이터를 가져오는 React Query 훅을 사용
-const useUserData = (page: number, pageSize: number, sort: string, accessToken: string) => {
-  return useQuery(['userData', page, pageSize, sort], () => fetchUserData(page, pageSize, sort, accessToken), {
+const useUserData = (accessToken: string) => {
+  return useQuery(['userData'], () => fetchUserData(accessToken), {
     // 캐싱 설정: 데이터를 캐싱하여 중복 요청을 방지
     staleTime: 60000,
     // 자동 재요청 설정: 일시적인 네트워크 에러에 대비하여 자동 재요청을 활성화
