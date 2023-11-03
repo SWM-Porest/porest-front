f2665b721015c52144f644790cc0138ffb85bdf6
kimhyun5u <gustndlfrl@gmail.com>
2023-11-03 12:25:45 +0900

    fix: socket.io transports

--- src/Socket/socketio.ts
+++ src/Socket/socketio.ts
@@ -1,8 +1,8 @@
 import { io, Socket } from 'socket.io-client'
 
-export const socket: Socket = io(`${process.env.REACT_APP_API_URL}/orders`)
-
 export const onOrder = (restaurant_id: string, callback: (data: any) => void) => {
+  const socket: Socket = io(`${process.env.REACT_APP_API_URL}/orders`, { transports: ['websocket'] })
+
   if (socket.hasListeners(restaurant_id)) socket.off(restaurant_id)
   socket.on(restaurant_id, callback)
 }
