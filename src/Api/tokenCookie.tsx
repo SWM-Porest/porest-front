f5edf077f5ccf6faee943d81c84cd713c3192eb1
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-11-06 09:48:42 +0900

    feat: 로그아웃 기능 추가

--- src/Api/tokenCookie.tsx
+++ src/Api/tokenCookie.tsx
@@ -48,3 +48,7 @@ export const getRefreshToken = () => {
     // console.log(err)
   }
 }
+export const logout = () => {
+  document.cookie = 'access_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+  document.cookie = 'refresh_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;'
+}
