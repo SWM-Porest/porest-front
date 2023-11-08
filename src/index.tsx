23482132db9a6ce9a92ee92f8b0e8bc372fa6d72
jiyoung choi <104538679+jiyoung00000@users.noreply.github.com>
2023-11-08 09:54:57 +0900

    Revert "fix: index 사용안하는 코드 제거"

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,23 @@
 import { CookiesProvider } from 'react-cookie'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'
 import axios from 'axios'
+import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: 'porest-1fe07.firebaseapp.com',
+  projectId: 'porest-1fe07',
+  storageBucket: 'porest-1fe07.appspot.com',
+  messagingSenderId: '429274896579',
+  appId: '1:429274896579:web:220fb3b054ac95125e2a76',
+  measurementId: 'G-NWNSZKYZVP',
+}
+
+const app = initializeApp(firebaseConfig)
+const analytics = getAnalytics(app)
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
@@ -10,4 +26,5 @@ root.render(
   </CookiesProvider>,
 )
 
+serviceWorkerRegistration.register()
 axios.defaults.withCredentials = true
